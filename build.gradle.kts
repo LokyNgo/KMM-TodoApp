cc6484407ccf3a481167494d5e79e55b5124dd19
LokyNgo <loky@bookshare.app>
2023-05-08 09:54:55 +0700

    Initial

---
+++ build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        mavenLocal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    afterEvaluate {
+        // Workaround for https://youtrack.jetbrains.com/issue/KT-52776
+        rootProject.extensions.findByType<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension>()?.apply {
+            versions.webpackCli.version = "4.10.0"
+        }
+    }
+}
